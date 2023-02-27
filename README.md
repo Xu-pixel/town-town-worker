9ab61ebdd04d3a4b12e536705e566b2b0899015b
Xu-pixel <naomi1299@163.com>
2023-02-27 10:34:03 +0800

    feat: 获取订单

--- README.md
+++ README.md
@@ -13,5 +13,7 @@ docker build . -t town_server
 docker run -d --net town -p 8000:8000 -e MONGO_URL=mongodb://mongo:27017 town_server
 
 # 数据库 监控
-docker run -d --net town -e ME_CONFIG_MONGODB_SERVER=mongo -e ME_CONFIG_BASICAUTH_USERNAME="user" -e ME_CONFIG_BASICAUTH_PASSWORD="zsy8081" -p 8081:8081 mongo-express
+docker run -d --net town -e ME_CONFIG_MONGODB_SERVER=mongo -p 8081:8081 mongo-express
 ```
+
+实际部署时记得添加必要的环境变量
--- routes/order.ts
+++ routes/order.ts
@@ -1,6 +1,8 @@
+import { getQuery } from "https://deno.land/x/oak@v11.1.0/helpers.ts";
 import { Router, Status } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { SessionGuard } from "../Middlewares.ts";
 import { Order, OrderModel } from "../models/Order.ts";
+import { z } from "https://deno.land/x/zod@v3.20.2/mod.ts";
 
 const router = new Router();
 export default router;
@@ -15,5 +17,22 @@ router
       formData.uid = state.userId
       const order = await OrderModel.create(formData)
       response.body = order
+    }
+  )
+  .get(
+    "/all",
+    async (ctx) => {
+      const params = getQuery(ctx, { mergeParams: true })
+      const page = z.coerce.number().parse(params.page || 0)
+      const size = z.coerce.number().parse(params.size || 10)
+      ctx.response.body = await OrderModel.find()
+        .skip(page * size)
+        .limit(size)
+    }
+  )
+  .get(
+    "/:id",
+    async ({ response, params }) => {
+      response.body = await OrderModel.findById(params.id)
     }
   )
