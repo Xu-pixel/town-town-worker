bd15bca5e664257455d5112542ac793cec6a92d7
Xu-pixel <naomi1299@163.com>
2023-03-02 16:53:11 +0800

    feat: 好麻好麻

--- models/Message.ts
+++ models/Message.ts
@@ -1,11 +1,10 @@
 import { ModelOptions, prop, getModelForClass } from "npm:@typegoose/typegoose@9.13.x";
-import { Order } from './Order.ts'
 import type { Ref } from "npm:@typegoose/typegoose@9.13.x"
 
 @ModelOptions({ schemaOptions: { timestamps: true } })
 export class Message {
-  @prop({ ref: () => Order })
-  rid: Ref<Order>;
+  @prop()
+  rid!: string
 
   @prop()
   content!: string
